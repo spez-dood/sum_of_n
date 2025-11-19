241dca82171f666cf94df0a995f300596c53779a
Spez <jluke5664@gmail.com>
2025-11-18 23:16:34 -0800

    Reduced redundancy
    Condensed:
    - combined dbsp and spacer1 into a single pointer
    - removed last_sum, opting to print before adding n

--- odd_n.cpp
+++ odd_n.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 
 const char* stSqr = "\u00B2"; // unicode for ^2
-const char* dbsp = "\n\n";
-const char* spacer1 = "===========================";
+const char* spacer1 = "\n===========================\n\n";
 
 struct odd_n {
     int sum = 0;
-    int last_sum = 0;
     int pos_n = 1;
 
     void print_sum(int n) {
         std::cout << "position: " << pos_n << "\n"
-                  << "current sum: " << last_sum << "\n"
                   << "number: " << n << "\n"
-                  << "sum: " <<  last_sum << " + " << n << " = " << sum << "\n"
-                  << "conclusion: " << sum << " = " << pos_n << stSqr
-                  << dbsp << spacer1 << dbsp;
+                  << "sum: " << sum << " + " << n << " = " << sum+n << "\n"
+                  << "conclusion: " << sum+n << " = " << pos_n << stSqr << "\n"
+                  << spacer1;
     }
 
     /* generate sum of pos(n) based on a single end-goal number.   
@@ -24,14 +21,10 @@ struct odd_n {
     odd_n(int end_n = 9) {
         if (end_n <= 0) { return; } // n must be positive.
 
-        for (int i = 0; i < end_n; ++i) {
-            if ( (i == 0) || (i%2 == 0) ) { continue; }
-
-            sum += i;
-
-            print_sum(i);
-
-            last_sum = sum;
+        for (int n = 0; n < end_n; ++n) {
+            if ( (n == 0) || (n%2 == 0) ) { continue; }
+            print_sum(n);
+            sum += n;
             pos_n++;
         }
     }
@@ -40,15 +33,15 @@ struct odd_n {
 
 int main(int argc, char const* argv[]) {
     std::cout << "\nThe sum of all odd numbers n = pos(n)" << stSqr << "\n"
-              << dbsp << spacer1 << dbsp
+              << spacer1
               << "default : odd_n()\n"
               << "~~< NUMBERS 1 - 9 >~~" 
-              << dbsp << spacer1 << dbsp;
+              << spacer1;
     odd_n();
 
     std::cout << "~~< NUMBERS 1 - 29 >~~\n"
               << "goal number : odd_n(int end_n)\n"
-              << dbsp << spacer1 << dbsp;
+              << spacer1;
     odd_n(30);
 
     return 0;
