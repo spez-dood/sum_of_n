32d4fe6d98bca6fcad50b9ec1bf325e1dde9d4d9
Spez <jluke5664@gmail.com>
2025-11-18 23:14:30 -0800

    Reduced redundancy
    Condensed:
    - combined dbsp and spacer1 into a single pointer
    - removed last_sum, opting to print before adding n

--- even_n.cpp
+++ even_n.cpp
@@ -1,36 +1,29 @@
 #include <iostream>
 
-const char* dbsp = "\n\n";
-const char* spacer1 = "===========================";
+const char* spacer1 = "\n===========================\n\n";
 
 struct even_n {
     int sum = 0;
-    int last_sum = 0;
     int pos_n = 1;
 
     void print_sum(int n) {
         std::cout << "position: " << pos_n << "\n"
-                  << "current sum: " << last_sum << "\n"
                   << "number: " << n << "\n"
-                  << "sum: " <<  last_sum << " + " << n << " = " << sum << "\n"
-                  << "conclusion: " << sum << " = " << pos_n << " x " << pos_n+1
-                  << dbsp << spacer1 << dbsp;
+                  << "sum: " << sum << " + " << n << " = " << sum+n << "\n"
+                  << "conclusion: " << sum+n << " = " << pos_n << " x " << pos_n+1 << "\n"
+                  << spacer1;
     }
 
     /* generate sum of pos(n) based on a single end-goal number.   
-    if not n, default to first 5 odd integers. */
+    if not n, default to first 5 even integers. */
 
     even_n(int end_n = 10) {
         if (end_n <= 0) { return; } // n must be positive.
 
-        for (int i = 0; i <= end_n; ++i) {
-            if ( (i == 0) || (i%2 != 0) ) { continue; }
-
-            sum += i;
-
-            print_sum(i);
-
-            last_sum = sum;
+        for (int n = 0; n < end_n; ++n) {
+            if ( (n == 0) || (n%2 != 0) ) { continue; }
+            print_sum(n);
+            sum += n;
             pos_n++;
         }
     }
@@ -38,16 +31,16 @@ struct even_n {
 };
 
 int main(int argc, char const* argv[]) {
-    std::cout << "\nThe sum of all even numbers n = pos(n)" << " x (pos(n)+1)\n"
-              << dbsp << spacer1 << dbsp
+    std::cout << "\nThe sum of all even numbers n = pos(n) x (pos(n)+1)\n"
+              << spacer1
+              << "~~< NUMBERS 2 - 10 >~~\n"
               << "default : even_n()\n"
-              << "~~< NUMBERS 2 - 10 >~~" 
-              << dbsp << spacer1 << dbsp;
+              << spacer1;
     even_n();
 
     std::cout << "~~< NUMBERS 2 - 30 >~~\n"
               << "goal number : even_n(int end_n)\n"
-              << dbsp << spacer1 << dbsp;
+              << spacer1;
     even_n(30);
 
     return 0;
